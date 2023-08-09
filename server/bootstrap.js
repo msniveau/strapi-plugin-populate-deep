f5b3465624d97d2842b2c521fbc4c103a60c955f
Bastian Heitmann <msniveau.bitbucket@movinto.net>
2023-08-09 12:27:03 +0200

    Update bootstrap.js
    Fix populate params

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -5,7 +5,9 @@ module.exports = ({ strapi }) => {
   // Subscribe to the lifecycles that we are intrested in.
   strapi.db.lifecycles.subscribe((event) => {
     if (event.action === 'beforeFindMany' || event.action === 'beforeFindOne') {
-      const populate = event.params?.populate;
+      const ctx = strapi.requestContext.get();
+      const urlParams = new URLSearchParams(ctx?.originalUrl.split('?')[1]);
+      const populate = urlParams.get("populate")?.split(",");
       const defaultDepth = strapi.plugin('strapi-plugin-populate-deep')?.config('defaultDepth') || 5
 
       if (populate && populate[0] === 'deep') {
